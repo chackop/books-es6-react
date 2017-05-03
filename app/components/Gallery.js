888c97b80798d2520fe3103cbb933812966576fd
chax <chackop@gmail.com>
2017-05-03 18:51:04 +0100

    updated with more book info
    updated with more book info

--- app/components/Gallery.js
+++ app/components/Gallery.js
@@ -2,20 +2,25 @@ import React, { Component } from 'react';
 
 class Gallery extends Component {
     render() {
+        let altimg = 'http://www.landmarkcu.com/images/security/warning_icon_big.png?sfvrsn=3';
+
         return {
             <div >
             {
                 this.props.items.map(item, index => {
                     let { title, imageLinks, infoLinks } = item.volumeInfo;
                     return {
-                        <div key= { index } >
-                        <img src={imageLinks.thumbnail} alt="book image">
-                            {title}
+                        <div key= { index } className="book">
+                            <img 
+                            src={imageLinks != undefined ? imageLinks.thumbnail : altimg } 
+                            alt="book image" 
+                            className="book-img">
+                            <div className="book-text">{title}</div>                            
                         </div>
-                }
+                    }
                 })
-    }
-    Gallery Component
+            }
+            Gallery Component
             </div>
         }
     } 
