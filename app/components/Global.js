96e53f7ade2037680df898f5277f9f08ad400041
chax <chackop@gmail.com>
2017-05-03 16:00:46 +0100

    event mapping
    event mapping

--- app/components/Global.js
+++ app/components/Global.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class Global extends Component {
+constructor(props) {
+    super(props);
+    this.state = {
+        query: ''
+    }
+}
+
     search() {
-        console.log('test search');
+        console.log('test search', this.state.query);
     }
 
     render() {
@@ -12,7 +19,15 @@ class Global extends Component {
             <h2> Book store</h2>
             <FormGroup>
                 <InputGroup>
-                    <FormControl type="text" placeholder="Enter book" />
+                    <FormControl type="text" 
+                    placeholder="Enter book" 
+                    onChange={event => this.setState({query: event.target.value})} 
+                    onKeyPress={event => {
+                        if (event.key === 'Enter') {
+                            this.search();
+                        }
+                    }}
+                    />
                     <InputGroup.Addon onClick={() => this.search()}>
                         <Glyphicon glyph="search">
                         </Glyphicon>
